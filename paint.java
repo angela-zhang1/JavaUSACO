d587bb4a08a093912f92d57a904d194d29063ad4
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-03-20 10:58:13 -0400

    Update paint.java

--- paint.java
+++ paint.java
@@ -21,17 +21,20 @@ public class paint {
 		int b0 = Integer.parseInt(st.nextToken());
 		int b1 = Integer.parseInt(st.nextToken());
 		int painted = (f1-f0)+(b1-b0);
-		if (b0 <f0 && b1 > f0) {
+		if (b0 <f0 && b1 > f0 && b1 < f1) {
 			painted -= b1;
 			painted += f0;
 		}
-		else if (f0 < b0 && f1 > b0) {
+		else if (f0 < b0 && f1 > b0 && f1 > b1) {
 			painted -= f1;
 			painted += b0;
 		}
-		else {
+		else if (b0 == f0 || b1 == f1) {
 			painted = Math.max((b1-b0), (f1-f0));
 		}
+		else if (b0<f0 && b1>f1 || f0<b0 && f1>b1)
+			painted = Math.max((b1-b0), (f1-f0));
+
 		pw.println(painted);
 		br.close();
 		pw.close();
