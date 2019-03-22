c82a427329e0d93057735302f11f29e0416b8662
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-03-22 11:15:45 -0400

    Create teleport.java

---
+++ teleport.java
@@ -0,0 +1,21 @@
+/*
+3
+-5 -7
+-3 10
+-2 7
+ans:
+10
+ */
+import java.io.*;
+import java.util.*;
+public class teleport {
+	public static void main (String[]args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("teleport.in"));
+		PrintWriter pw = new PrintWriter(new BufferedWriter (new FileWriter ("teleport.out")));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int portals = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+
+
+	}
+}
