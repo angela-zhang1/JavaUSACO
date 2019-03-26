01fc613665e55b301eb9c2ae820864397bd893cd
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-03-25 20:36:39 -0400

    Update teleport.java

--- teleport.java
+++ teleport.java
@@ -1,11 +1,7 @@
-/*
-3
--5 -7
--3 10
--2 7
+/*3 10 8 2
 ans:
-10
- */
+3
+*/
 import java.io.*;
 import java.util.*;
 public class teleport {
@@ -13,8 +9,21 @@ public class teleport {
 		BufferedReader br = new BufferedReader(new FileReader("teleport.in"));
 		PrintWriter pw = new PrintWriter(new BufferedWriter (new FileWriter ("teleport.out")));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int portals = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
+		int left = Integer.parseInt(st.nextToken());
+		int right = Integer.parseInt(st.nextToken());
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int temp;
+		
+		int l = Math.min(left, right);
+		int r = Math.max(left, right);
+		temp = Math.min(x, y);
+		y = Math.max(x, y);
+		x = temp;
+		
+		pw.println(Math.min((y-x), Math.abs(x-l)+Math.abs(y-r)));
+		br.close();
+		pw.close();
 
 
 	}
