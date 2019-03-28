bee6fb86b05da253b0d8cb894d5b88814d0b8c07
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-03-28 17:12:49 -0400

    Update teleport.java

--- teleport.java
+++ teleport.java
@@ -2,26 +2,37 @@
 ans:
 3
 */
+
 import java.io.*;
 import java.util.*;
+
+//USACO Bronze 2018 February contest, problem: teleport
+
 public class teleport {
 	public static void main (String[]args) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader("teleport.in"));
 		PrintWriter pw = new PrintWriter(new BufferedWriter (new FileWriter ("teleport.out")));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int left = Integer.parseInt(st.nextToken());
-		int right = Integer.parseInt(st.nextToken());
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
-		int temp;
+		int x0 = Integer.parseInt(st.nextToken());
+		int y0 = Integer.parseInt(st.nextToken());
+		int x1 = Integer.parseInt(st.nextToken());
+		int y1 = Integer.parseInt(st.nextToken());
+		int ret = Math.abs(y0-x0);
 		
+		ret = Math.min(ret, Math.abs(x1-x0) + Math.abs(y1-y0));
+		
+		ret = Math.min(ret, Math.abs(y1-x0) + Math.abs(y0-x1));
+		
+		 /*
 		int l = Math.min(left, right);
 		int r = Math.max(left, right);
 		temp = Math.min(x, y);
 		y = Math.max(x, y);
 		x = temp;
+		*/
 		
-		pw.println(Math.min((y-x), Math.abs(x-l)+Math.abs(y-r)));
+		//pw.println(Math.min((y-x), Math.abs(x-l)+Math.abs(y-r)));
+		pw.println(ret);
 		br.close();
 		pw.close();
 
