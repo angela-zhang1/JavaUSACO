21734f5c1eb5781c01bedde3b1417f6457c3eed1
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-04-08 20:14:28 -0400

    Update diamond.java

--- diamond.java
+++ diamond.java
@@ -1,2 +1,12 @@
 import java.io.*;
-import java.utils.*;
+import java.util.*;
+
+public class diamond {
+ 	public static void main(String[]args) {
+		BufferedReader br = new BufferedReader(new FileReader("diamond.in"));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Printwriter pw = new PrintWriter(new BufferedWriter(new FileWriter("diamond.out")));
+		
+		int x = 
+	}
+}
