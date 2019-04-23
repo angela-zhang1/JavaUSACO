b1a80864ae45983596312e47186d711b6be36289
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-04-22 22:56:01 -0400

    Update cowqueue.java

--- cowqueue.java
+++ cowqueue.java
@@ -16,8 +16,10 @@ public class cowqueue {
 		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		//PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("cowqueue.out")));
-		int x = Integer.parseInt(st.nextToken()); int z; int a; int ret = 0;
-		int[][] y = new int[x][2];
+		int x = Integer.parseInt(st.nextToken()); 
+		//int z; int a; 
+		int ret = 0;
+		/*int[][] y = new int[x][2];
 		int[]b = new int[x];
 		for (int i = 0; i < x; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -25,10 +27,29 @@ public class cowqueue {
 			a = Integer.parseInt(st.nextToken());
 			y[i][0] = z;
 			y[i][1] = a;
+			b[z] = a;
 		}
 		for (int i = 1; i <x; i++) {
-
+			
 		}
-
+		*/
+		int[] y = new int[1000000];
+		int z; int a;
+		for (int i = 0; i < x; i++) {
+			st = new StringTokenizer(br.readLine());
+			z = Integer.parseInt(st.nextToken());
+			a = Integer.parseInt(st.nextToken());
+			for (int j = z; j < z+a; j++) {
+				y[i]++;
+				ret+=a;
+			}
+		}
+		for (int i = 0; i < 1000000; i++) {
+			if (y[i] > 1) {
+				ret+=y[i] - 1;
+			}
+		}
+		System.out.println(ret);
+		//pw.close();
 	}
 }
