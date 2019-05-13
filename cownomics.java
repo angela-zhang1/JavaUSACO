f6e258c7a2dd3ac37ee60487fbd265c8531e126c
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-05-13 16:34:01 -0400

    Create cownomics.java

---
+++ cownomics.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class cownomics {
+  public static void main (String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new FileReader("cownomics.in"));
+	StringTokenizer st = new StringTokenizer(br.readLine());
+	PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("cownomics.out")));
+  }
+}
