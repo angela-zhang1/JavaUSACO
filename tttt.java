c675f2aec17dd6616c53bd1ac04994d2ab7bbe4d
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-05-14 19:56:10 -0400

    Create tttt.java

---
+++ tttt.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class tttt {
+	public static void main (String[]args) throws IOException {
+		//BufferedReader br = new BufferedReader(new FileReader("tttt.in"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		//PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("tttt.out")));
+	
+	}
+}
