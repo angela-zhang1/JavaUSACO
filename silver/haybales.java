0047e0c2a97524d4e47c11caa8a32dfff7346fbc
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-06-12 16:04:24 -0400

    Update and rename haybales.java to silver/haybales.java

--- silver/haybales.java
+++ silver/haybales.java
@@ -49,18 +49,30 @@ ans:
    
 */
 
+//USACO Silver 2016 December contest, problem: haybales
+
 public class haybales {
+	static int []a;
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new FileReader("haybales.in"));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("haybales.out")));
 		int n = Integer.parseInt(br.readLine());
 		int q = Integer.parseInt(br.readLine());
+		a = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < q; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			pw.println(bsearch(y) - besarch(x-1));
+		}
+		pw.close();
 	}
 	static int bsearch(int val) {
 		if (a[0] > val)
 			return 0;
-		int lo = 0; hi = a.length-11;
+		int lo = 0; hi = a.length-1;
 		while (lo!= hi) {
 			int mid = (lo+hi+1)/2;
 			if (a[mid] <= val)
