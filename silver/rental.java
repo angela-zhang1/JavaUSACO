00a661f21090bd88fef9c1b8187343e1a8d1fd11
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-09-20 16:56:22 -0400

    Create rental.java

---
+++ silver/rental.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+/*
+5 3 4
+6
+2
+4
+7
+1
+10 25
+2 10
+15 15
+250
+80
+100
+40
+ans:
+725
+*/
+
+public class rental {
+	public static void main (String[] args) throws Exception {
+		//BufferedReader br = new BufferedReader(new FileReader("rental.in"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//PrintWriter pw = new PrintWriter(new FileWriter("rental.out"));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		PriorityQueue <Integer> c = new PriorityQueue<Integer> (); //cow milk production
+		PriorityQueue <Integer> f = new PriorityQueue<Integer> (Collections.reverseOrder()); //farmer prices
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		state[]s = new state[m]; //store prices and stuff
+		int x = 0; int y = 0;
+		for (int i = 0; i < n; i++) { //cow milk
+			st = new StringTokenizer(br.readLine());
+			x = Integer.parseInt(st.nextToken());
+			c.add(x);
+		}
+		for (int i = 0; i < m; i++) { //store prices
+			st = new StringTokenizer(br.readLine());
+			x = Integer.parseInt(st.nextToken());
+			y = Integer.parseInt(st.nextToken());
+			s[i] = new state(x,y);
+		}
+		for (int i = 0; i < r; i++) { //farmer rental
+			x = Integer.parseInt(br.readLine());
+			f.add(x);
+		}
+		int ret = 0;
+	}
+	static class state implements Comparable<state>{
+		int g, p; //g = gallons accepted, p = price
+		public state (int gall, int price) { 
+			g = gall;
+			p = price;
+		}
+		public int compareTo(state o) {
+			if (o.p > this.p) {
+				return 1;
+			}
+			else if (o.p < this.p) {
+				return -1;
+			}
+			else {
+				return 0;
+			}
+		}
+	}
+}
