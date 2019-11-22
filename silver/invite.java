4cfeb9365891ac2d7a0851b7b2ca02061f3d254f
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-11-22 11:41:30 -0500

    Create invite.java

---
+++ silver/invite.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class invite {
+	public static void main (String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//BufferedReader br = new BufferedReader(new FileReader("invite.in"));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+	}
+}
