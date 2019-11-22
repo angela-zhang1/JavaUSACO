250cab040724a98dbae85290cc7e9291fc42bccd
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-11-22 15:59:39 -0500

    Update invite.java

--- silver/invite.java
+++ silver/invite.java
@@ -18,12 +18,14 @@ public class invite {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int g = Integer.parseInt(st.nextToken());
-		int x;
+		boolean[][] a = new boolean[g][n];
+		int x; int y;
 		for (int i = 0; i < g; i++) {
 			st = new StringTokenizer(br.readLine());
 			x = Integer.parseInt(st.nextToken());
 			for (int j = 0; j < x; j++) {
-
+				y = Integer.parseInt(st.nextToken());
+				a[i][y] = true;
 			}
 		}
 	}
