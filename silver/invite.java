47dc360e51b8f5e329056657e03752ae3026f273
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-11-22 12:34:01 -0500

    Update invite.java

--- silver/invite.java
+++ silver/invite.java
@@ -1,11 +1,30 @@
 import java.util.*;
 import java.io.*;
 
+/*
+10 4
+2 1 3
+2 3 4
+6 1 2 3 4 6 7
+4 4 3 2 1
+ans:
+4
+*/
+
 public class invite {
 	public static void main (String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		//BufferedReader br = new BufferedReader(new FileReader("invite.in"));
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int g = Integer.parseInt(st.nextToken());
+		int x;
+		for (int i = 0; i < g; i++) {
+			st = new StringTokenizer(br.readLine());
+			x = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < x; j++) {
+
+			}
+		}
 	}
 }
