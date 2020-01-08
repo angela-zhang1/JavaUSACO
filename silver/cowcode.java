c1fdc7733fb311ec7f35828f5c0ac717503db69b
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2020-01-08 13:41:07 -0500

    Update cowcode.java

--- silver/cowcode.java
+++ silver/cowcode.java
@@ -11,8 +11,12 @@ C
 COW|WCO|OCOWWC|CCOWWCOOCOWW|
 
  n    n      2n           4n
-1234 2341 11234234 4123423411123423
-ABCD|BCDA|AABCDBCD|DABCDBCDAAABCDBC|
+1234 4123 31234412
+ABCD|DABC|CABCDDAB
+
+recursive, just do it, it'll be O(log n)
+
+123||123|123||
 */
 
 public class cowcode {
@@ -26,8 +30,11 @@ public class cowcode {
 		long n = Integer.parseInt(st.nextToken());
 		br.close();
 		int x = s.length();
+		int y = (int) (Math.log(n/x)/Math.log(2)) + 1;
 
-
-
+	}
+	public char function (int x) {
+		
+		return 'c';
 	}
 }
