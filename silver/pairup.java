37d1c00da808e193f81e972332de33cbd4aa1a6c
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2020-01-31 13:36:31 -0500

    Update pairup.java

--- silver/pairup.java
+++ silver/pairup.java
@@ -10,6 +10,8 @@ ans:
 10
 */
 
+//2017 US open silver
+
 public class pairup {
 	public static void main (String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new FileReader("pairup.in"));
