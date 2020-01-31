5b96b574442bd904342b3a92f5097a1f6dc2afdb
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2020-01-31 13:36:10 -0500

    Create pairup.java

---
+++ silver/pairup.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+/*
+3
+1 8
+2 5
+1 2
+ans:
+10
+*/
+
+public class pairup {
+	public static void main (String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new FileReader("pairup.in"));
+	}
+}
