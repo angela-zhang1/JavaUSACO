e33159462a73fc6b87c1686e22ea5e19f2a917db
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2020-02-12 13:50:48 -0500

    Create gpsduel.java

---
+++ silver/gpsduel.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class gpsduel {
+	public static void main (String[] args) throws Exception {
+		//BufferedReader br = new BufferedReader(new FileReader("gpsduel.in"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("gpsduel.out")));
+	}
+}
