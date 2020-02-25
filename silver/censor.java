52edfe1c53f3f3ac3eb0fb6bf88824c0e4b8291d
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2020-02-25 13:10:15 -0500

    Create censor.java

---
+++ silver/censor.java
@@ -0,0 +1,20 @@
+  import java.io.*;
+import java.util.*;
+
+/*
+whatthemomooofun
+moo
+ans:
+whatthefun
+*/
+
+public class censor {
+	public static void main (String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new FileReader("censor.java"));
+		String s;
+		String t;
+		s = br.readLine();
+		t = br.readLine();
+		int x = s.length();
+	}
+}
